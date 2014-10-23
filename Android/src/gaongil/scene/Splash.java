2a1bf146845997774eda7c451bd3ba6f4da9edfa
Yoonsung <lvev9925@naver.com>
2014-10-23 16:36:47 +0900

    Create Android Project

---
+++ Android/src/gaongil/scene/Splash.java
@@ -0,0 +1,15 @@
+package gaongil.scene;
+
+import gaongil.safereturnhome.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+	}
+	
+}
