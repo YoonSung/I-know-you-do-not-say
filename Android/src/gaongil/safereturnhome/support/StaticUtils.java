39f2d2b73112e08d20a31d5f13f05dfeae1b7dad
Yoonsung <lvev9925@naver.com>
2014-11-25 11:19:41 +0900

    Add Image Resize Static method

--- Android/src/gaongil/safereturnhome/support/StaticUtils.java
+++ Android/src/gaongil/safereturnhome/support/StaticUtils.java
@@ -1,6 +1,8 @@
 package gaongil.safereturnhome.support;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 import org.apache.http.HttpEntity;
@@ -13,37 +15,45 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
+import android.provider.MediaStore.Images;
 import android.view.Gravity;
 import android.widget.Toast;
 
 public class StaticUtils {
-	
-	//upload file to server
-	//http://stackoverflow.com/questions/11044291/using-httppost-to-upload-file-android
-	public static String postRequest(String url, List<NameValuePair> parameters) throws ClientProtocolException, IOException {
+
+	// upload file to server
+	// http://stackoverflow.com/questions/11044291/using-httppost-to-upload-file-android
+	public static String postRequest(String url, List<NameValuePair> parameters)
+			throws ClientProtocolException, IOException {
 		HttpPost httppost = new HttpPost(Constant.ROOT_PATH + url);
-		
+
 		if (parameters != null)
 			httppost.setEntity(new UrlEncodedFormEntity(parameters));
-		
+
 		HttpResponse response = new DefaultHttpClient().execute(httppost);
 		HttpEntity entity = response.getEntity();
-		
+
 		if (response != null && entity != null)
 			return EntityUtils.toString(entity);
 		else
 			return null;
 	}
-	
+
 	public static void centerToast(Context context, String message) {
 		Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
 		toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL,
 				0, 0);
 		toast.show();
 	}
-	
+
 	public static boolean checkNetwork(Context context) {
 		ConnectivityManager cm = (ConnectivityManager) context
 				.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -55,5 +65,41 @@ public class StaticUtils {
 			return true;
 		centerToast(context, "네트워크 연결에 실패하였습니다.\n인터넷 연결상태를 확인해 주세요.");
 		return false;
+	}
+
+	/**
+	 * Scales the provided bitmap to have the height and width provided.
+	 * (Alternative method for scaling bitmaps since
+	 * Bitmap.createScaledBitmap(...) produces bad (blocky) quality bitmaps.)
+	 * 
+	 * @param bitmap
+	 *            is the bitmap to scale.
+	 * @param newWidth
+	 *            is the desired width of the scaled bitmap.
+	 * @param newHeight
+	 *            is the desired height of the scaled bitmap.
+	 * @return the scaled bitmap.
+	 * @throws IOException 
+	 * @throws FileNotFoundException 
+	 */
+	public static Bitmap scaleBitmap(Context context, Uri ImageUri, int newWidth, int newHeight) throws FileNotFoundException, IOException {
+		
+		Bitmap bitmap = Images.Media.getBitmap(context.getContentResolver(), ImageUri);
+		
+		Bitmap scaledBitmap = Bitmap.createBitmap(newWidth, newHeight, Config.ARGB_8888);
+
+		float scaleX = newWidth / (float) bitmap.getWidth();
+		float scaleY = newHeight / (float) bitmap.getHeight();
+		float pivotX = 0;
+		float pivotY = 0;
+
+		Matrix scaleMatrix = new Matrix();
+		scaleMatrix.setScale(scaleX, scaleY, pivotX, pivotY);
+
+		Canvas canvas = new Canvas(scaledBitmap);
+		canvas.setMatrix(scaleMatrix);
+		canvas.drawBitmap(bitmap, 0, 0, new Paint(Paint.FILTER_BITMAP_FLAG));
+
+		return scaledBitmap;
 	}
 }
