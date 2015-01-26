b52da1e600ac9418d345f4c55436a58a4643cae5
Yoonsung <lvev9925@naver.com>
2015-01-26 21:37:36 +0900

    Convert to gradle project. working well

---
+++ Android/app/src/main/java/gaongil/safereturnhome/scene/ChatActivity.java
@@ -0,0 +1,267 @@
+package gaongil.safereturnhome.scene;
+
+import gaongil.safereturnhome.R;
+import gaongil.safereturnhome.model.MessageData;
+import gaongil.safereturnhome.model.MessageType;
+import gaongil.safereturnhome.support.ChatAdapter;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.view.MenuItem;
+
+public class ChatActivity extends CustomActivity implements OnClickListener {
+	
+	/************************************************************************
+	 * Forground Check
+	 */
+	private static boolean VISIBILITY = false;
+	public static boolean isVisible() {
+		return ChatActivity.VISIBILITY;
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		ChatActivity.VISIBILITY = true;
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		ChatActivity.VISIBILITY = false;
+	}
+	/*
+	 * Forground Check
+	 *************************************************************************/
+	
+	/**
+	 * MainContents
+	 */
+	// Message List
+	private ArrayList<MessageData> mMessageList;
+	// Chat Custom Adapter.
+	private ChatAdapter mChatAdapter;
+	// EditText to compose the messages
+	private EditText mEditText;
+	// Button to submit new message
+	private Button mBtnSend;
+	// Control Keyboard Panel
+	private InputMethodManager inputMethodManager;
+	private ActionBarDrawerToggle mDrawerToggle;
+	
+	/**
+	 * The drawer layout
+	 */
+	private DrawerLayout mDrawerLayout;
+
+	/**
+	 * Left drawer
+	 */
+	private View mLeftDrawerView;
+
+	/**
+	 * Right drawer
+	 */
+	private View mRightDrawerView;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_chat);
+	    loadMessageList();
+	    init();
+	    
+	    setupActionBar("BF");
+	    setupDrawer();
+	    
+	    
+	    //TODO Complete Code
+	    setupLeftDrawer();
+	    setupRightDrawer();
+	    
+	    //TODO DELETE
+	    //Test Start
+	    setupTest();
+	  //TODO DELETE
+	    //Test End
+	}
+
+	//TODO DELETE
+    //Test Start
+	private void setupTest() {
+		mMessageList.add(new MessageData(0, 0, "[귀가시작] 집까지 같이해 주세요.위치정보, 센서정보 공유가 시작되었습니다.\n", new Date(1,1,1, 20, 51),  MessageType.ANNOUNCE, true));
+		mMessageList.add(new MessageData(0, 0, "[충격 위협감지] 갑작스러운\n 충격이 감지되었습니다.\n위치 : 성남시 분당구 삼평동 봇들공원\n !무슨일인지 확인해주세요!", new Date(1,1,1, 21, 03),  MessageType.URGENT, true));
+		mMessageList.add(new MessageData(0, 1, "무슨일이야??", new Date(1,1,1, 21, 03),  MessageType.NORMAL, false));
+		mMessageList.add(new MessageData(0, 4, "내가 전화해볼게", new Date(1,1,1, 21, 04),  MessageType.ANNOUNCE, false));
+		mMessageList.add(new MessageData(0, 0, "아.. 괜찮아! 딴데보다가 살짝 넘어졌어ㅠ", new Date(1, 1, 1, 21, 04),  MessageType.NORMAL, true ));
+		mMessageList.add(new MessageData(0, 3, "으이구.. 괜찮아?\n데릴러 나갈게", new Date(1, 1, 1, 21, 04),  MessageType.NORMAL, false));
+		mMessageList.add(new MessageData(0, 0, "응응! 고마워어ㅠㅠㅠ", new Date(1, 1, 1, 21, 05),  MessageType.NORMAL, true ));
+		
+		mChatAdapter.notifyDataSetChanged();
+
+	}
+	//TODO DELETE
+    //Test End
+	
+	private void setupActionBar(String groupName) {
+		super.setupActionBar(R.drawable.ic_location);
+		centerActionBarTitle(groupName);
+	}
+	
+	private void centerActionBarTitle(String groupName) {
+
+		ActionBar actionBar = getSupportActionBar();
+		if (actionBar == null)
+			return;
+		actionBar.setDisplayShowTitleEnabled(true);
+		actionBar.setTitle(groupName);
+		
+        int titleId = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+        {
+            titleId = getResources().getIdentifier("action_bar_title", "id", "android");
+        }
+        else
+        {
+            // This is the id is from your app's generated R class when ActionBarActivity is used 
+            // for SupportActionBar
+            //titleId = R.id.action_bar_title;
+        }
+
+        // Final check for non-zero invalid id
+        if (titleId > 0)
+        {
+            TextView titleTextView = (TextView) findViewById(titleId);
+            DisplayMetrics metrics = getResources().getDisplayMetrics();
+
+            // Fetch layout parameters of titleTextView (LinearLayout.LayoutParams : Info from HierarchyViewer)
+            LinearLayout.LayoutParams txvPars = (android.widget.LinearLayout.LayoutParams) titleTextView.getLayoutParams();
+            txvPars.gravity = Gravity.CENTER_HORIZONTAL;
+            txvPars.width = metrics.widthPixels;
+            titleTextView.setLayoutParams(txvPars);
+
+            titleTextView.setGravity(Gravity.CENTER);
+        }
+    }
+
+	private void init() {
+		 // Chatting ListView Setting
+	    ListView list = (ListView)findViewById(R.id.chat_list);
+	    mChatAdapter = new ChatAdapter(ChatActivity.this, mMessageList);
+	    
+	    list.setAdapter(mChatAdapter);
+	    list.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+	    list.setStackFromBottom(true);
+	    
+	    // Chat Input Box Init
+	    mEditText = (EditText) findViewById(R.id.chat_edt);
+	    
+	    // Submit Button init
+	    mBtnSend = (Button) findViewById(R.id.chat_btn_send);
+	    mBtnSend.setOnClickListener(this);
+	    
+	    // inputMethodManager init
+	    inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+	}
+
+	private void loadMessageList() {
+		
+		// TODO getDataFrom Server or Local DB
+		mMessageList = new ArrayList<MessageData>();
+		
+		mMessageList.add(new MessageData(
+				1, 1, "test", new Date(),  MessageType.NORMAL, true 
+		));
+	}
+
+	private void sendMessage() {
+		
+		if (mEditText.length() == 0)
+			return;
+		
+		inputMethodManager.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
+		
+		//TODO getGroupId & UserId & MessageType & etc..
+		MessageData newMessage = new MessageData(
+				1, 
+				1, 
+				mEditText.getText().toString(), 
+				new Date(), 
+				MessageType.NORMAL, 
+				false
+		);
+		
+		//TODO Post Message, and Send Success, then add chatList
+		mMessageList.add(newMessage);
+		
+		mChatAdapter.notifyDataSetChanged();
+		mEditText.setText(null);
+		
+	}
+	
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.chat_btn_send:
+			sendMessage();
+			break;
+		}
+	}
+
+	@Override
+	protected boolean onRightDrawerToggleSelected(MenuItem item) {
+		return super.defaultRightDrawerToggleSelected();
+	}
+
+	@Override
+	protected void setupDrawer() {
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_chat_layout);
+		mLeftDrawerView = (View) findViewById(R.id.drawer_chat_left);
+		mRightDrawerView = (View) findViewById(R.id.drawer_chat_right);
+		LinearLayout mainContentLayout = (LinearLayout) findViewById(R.id.chat_content_layout);
+		
+		mDrawerToggle = super.getActionBarDrawerToggle(
+				this, 
+				mDrawerLayout, 
+				mLeftDrawerView, 
+				mRightDrawerView, 
+				mainContentLayout, 
+				R.drawable.ic_location,
+				R.menu.chat
+		);
+		
+		super.setDrawerLayoutOptions(mDrawerLayout, mDrawerToggle);
+		
+	}
+
+	@Override
+	protected void setupLeftDrawer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setupRightDrawer() {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ Android/app/src/main/java/gaongil/safereturnhome/scene/CustomActivity.java
@@ -0,0 +1,204 @@
+package gaongil.safereturnhome.scene;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.KeyEvent;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.view.Menu;
+import android.view.View;
+import android.view.animation.TranslateAnimation;
+import android.widget.LinearLayout;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+import com.actionbarsherlock.view.MenuItem;
+
+import gaongil.safereturnhome.R;
+import gaongil.safereturnhome.support.Constant;
+
+public abstract class CustomActivity extends SherlockFragmentActivity {
+
+    private ActionBarDrawerToggle leftDrawerToggle;
+    private int rightDrawerToggleDrawableId;
+    private DrawerLayout mDrawerLayout;
+    private View mRightDrawerView, mLeftDrawerView;
+
+    protected abstract boolean onRightDrawerToggleSelected(MenuItem item);
+
+    protected abstract void setupDrawer();
+
+    protected abstract void setupLeftDrawer();
+
+    protected abstract void setupRightDrawer();
+
+    protected void setupActionBar(int leftDrawerToggleDrawableId) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar == null)
+            return;
+
+        //mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setLogo(leftDrawerToggleDrawableId);
+        actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.main_color_blue)));
+
+        //Its default system menu graphical icon
+        //mActionBar.setDisplayHomeAsUpEnabled(true);
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+    }
+
+    //TODO DELETE drawableId
+    protected ActionBarDrawerToggle getActionBarDrawerToggle(
+            final Activity activity,
+            final DrawerLayout mDrawerLayout,
+            final View mLeftDrawerView,
+            final View mRightDrawerView,
+            final LinearLayout mainContentLayout,
+            final int drawerToggleDrawableId,
+            final int rightDrawerToggleDrawableId) {
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
+                GravityCompat.START);
+
+        this.rightDrawerToggleDrawableId = rightDrawerToggleDrawableId;
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(activity, mDrawerLayout,
+                drawerToggleDrawableId, 0, 0) {
+
+            // It's for lastTranslate Saved Variation
+            private float lastTranslate = 0.0f;
+
+            @Override
+            public void onDrawerClosed(View view) {
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+            }
+
+            @SuppressLint("NewApi")
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                // Its Drawer action Conflict Prevent Code
+                if (drawerView == mLeftDrawerView) {
+                    mDrawerLayout.closeDrawer(mRightDrawerView);
+                } else {
+                    mDrawerLayout.closeDrawer(mLeftDrawerView);
+                }
+
+                if (drawerView == mRightDrawerView) {
+                    // if rightDrawer Action, Opposite Direction Set
+                    slideOffset *= -1;
+                }
+
+                float moveFactor = (mDrawerLayout.getWidth()
+                        * Constant.DRAWER_SLIDE_WIDTH_RATE * slideOffset);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                    mainContentLayout.setTranslationX(moveFactor);
+                } else {
+                    TranslateAnimation anim = new TranslateAnimation(
+                            lastTranslate, moveFactor, 0.0f, 0.0f);
+                    anim.setFillAfter(true);
+                    mainContentLayout.startAnimation(anim);
+
+                    lastTranslate = moveFactor;
+                }
+            } //onDrawerSlide
+        }; //new ActionBarDrawerToggle
+
+
+        //saveData in Parent Because OptionItemSelected or etc actions
+        this.mDrawerLayout = mDrawerLayout;
+        this.leftDrawerToggle = actionBarDrawerToggle;
+        this.mRightDrawerView = mRightDrawerView;
+        this.mLeftDrawerView = mLeftDrawerView;
+
+        return actionBarDrawerToggle;
+    }
+
+    protected void setDrawerLayoutOptions(DrawerLayout mDrawerLayout, ActionBarDrawerToggle mDrawerToggle) {
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerLayout.closeDrawers();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (getSupportFragmentManager().getBackStackEntryCount() > 1) {
+                getSupportFragmentManager().popBackStackImmediate();
+            } else
+                finish();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater().inflate(rightDrawerToggleDrawableId, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        System.out.println("Menu Clicked!!!");
+        System.out.println(item.getItemId() == android.R.id.home);
+
+        //Left Drawer Toggle Clicked
+        //if (leftDrawerToggle.onOptionsItemSelected(item)) {
+        if (item.getItemId() == android.R.id.home) {
+            return onLeftDrawerToggleSelected(item);
+            //return true;
+
+        //Right Drawer Toggle Clicked
+        } else {
+            return onRightDrawerToggleSelected(item);
+        }
+
+        //return super.onOptionsItemSelected(item);
+    }
+
+    private boolean onLeftDrawerToggleSelected(MenuItem item) {
+        if (mDrawerLayout.isDrawerOpen(mLeftDrawerView)) {
+            mDrawerLayout.closeDrawer(mLeftDrawerView);
+        } else {
+            mDrawerLayout.openDrawer(mLeftDrawerView);
+        }
+
+        return true;
+    }
+
+    public boolean defaultRightDrawerToggleSelected() {
+        if (mDrawerLayout.isDrawerOpen(mRightDrawerView)) {
+            mDrawerLayout.closeDrawer(mRightDrawerView);
+        } else {
+            mDrawerLayout.openDrawer(mRightDrawerView);
+        }
+
+        return true;
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        leftDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggle
+        leftDrawerToggle.onConfigurationChanged(newConfig);
+    }
+}
