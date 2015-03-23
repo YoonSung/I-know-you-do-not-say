0414b02e0983053b2fe0b18a4848b99753d6d88a
Yoonsung <lvev9925@naver.com>
2015-03-23 18:06:45 +0900

    Remove obsolete Class files

--- WebServer/src/main/java/gaongil/controller/TestController.java
+++ WebServer/src/main/java/gaongil/controller/TestController.java
@@ -2,23 +2,19 @@ package gaongil.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
-@RequestMapping("/test")
+@Controller
+@RequestMapping("/")
 public class TestController {
 
 	private static final Logger log = LoggerFactory.getLogger(TestController.class);
 	
-	@RequestMapping("/{user}")
-	public String test(@PathVariable String user) {
-		log.info("testRequest User : {}", user);
-		
-		if (user == null)
-			return "false";
-		
-		return "true";
+	@RequestMapping("")
+	public @ResponseBody String home() {
+		log.info("/ : ROOT Request");
+		return "Server Operation Success!";
 	}
 }
